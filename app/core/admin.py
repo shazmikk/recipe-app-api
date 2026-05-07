0c2159aa39cfd13c7b5692daa8de41452c19b3eb
Shahzad Rafique <shazmikk@lovelltechnologies.com>
2026-05-07 22:30:48 +0500

    Build User API

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,37 @@
-from django.contrib import admin # noqa
+"""
+Django admin configuration for the core app.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+    readonly_fields = ('last_login',)
+
+admin.site.register(models.User, UserAdmin)
